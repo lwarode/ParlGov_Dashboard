4b2d92334407963a0ae243bc4bc3d1807902d4ad
lwarode <lukas.warode@gmx.de>
2021-04-16 13:19:15 +0200

    rearrangement of if statement

--- global.R
+++ global.R
@@ -1,9 +1,44 @@
-pg_logo_url <- "http://www.parlgov.org/static/images/parlgov-logo.svg"
+library(tidyverse)
 
-pg_logo_colors <- colorfindr::get_colors(pg_logo_url, 
-                                         exclude_col = "white", 
-                                         top_n = 5) %>% 
-  pull(col_hex)
+# ParlGov logo and colors -------------------------------------------------
+if (! "pg_logo.svg" %in% list.files()) {
+  
+  pg_logo_url <- "http://www.parlgov.org/static/images/parlgov-logo.svg"
+  
+  download.file(pg_logo_url, "pg_logo.svg")
+  
+  pg_color_1 <- colorfindr::get_colors("pg_logo.svg", 
+                                      exclude_col = "white", 
+                                      top_n = 5) %>% 
+    pull(col_hex) %>% 
+    pluck(1) # "#474749"
+  
+  pg_color_2 <- colorfindr::get_colors("pg_logo.svg", 
+                                       exclude_col = "white", 
+                                       top_n = 5) %>% 
+    pull(col_hex) %>% 
+    pluck(2) # "#2076B6"
+  
+} else {
+  
+  pg_color_1 <- colorfindr::get_colors("pg_logo.svg", 
+                                       exclude_col = "white", 
+                                       top_n = 5) %>% 
+    pull(col_hex) %>% 
+    pluck(1) # "#474749"
+  
+  pg_color_2 <- colorfindr::get_colors("pg_logo.svg", 
+                                       exclude_col = "white", 
+                                       top_n = 5) %>% 
+    pull(col_hex) %>% 
+    pluck(2) # "#2076B6"
+  
+}
 
-pg_color_1 <- pg_logo_colors %>% pluck(1) # "#474749"
-pg_color_2 <- pg_logo_colors %>% pluck(2) # "#2076B6"
+
+
+
+# data section ------------------------------------------------------------
+
+
+
--- party_color.R
+++ party_color.R
@@ -1,41 +1,35 @@
-library(tidyverse)
-library(partycoloR) # devtools::install_github("lwarode/partycoloR")
-
-
-# Party Facts ParlGov -----------------------------------------------------
-
-pf_url <- "https://partyfacts.herokuapp.com/download/external-parties-csv/"
-
-pf_raw <- read_csv(pf_url, guess_max = 30000)
-
-pf_pg <- pf_raw %>% 
-  filter(dataset_key == "parlgov")
-
-
-# Wikipedia Party URLs ----------------------------------------------------
-
-wiki_url <- "https://raw.githubusercontent.com/hdigital/partyfactsdata/master/import/wikipedia/wikipedia.csv"
-
-wiki_raw <- read_csv(wiki_url)
-
-wiki_pg <- wiki_raw %>% 
-  select(partyfacts_id, url) %>% 
-  right_join(pf_pg, by = "partyfacts_id")
-
-# wiki_pg_list <- wiki_pg %>% 
-#   filter(! is.na(url)) %>% 
-#   # pull(url)
-#   .$url %>% 
-#   as.list()
-
-# wiki_pg_list <- as.list(wiki_pg$url)
-
-wiki_pg_list <-  wiki_pg %>%
-  filter(! is.na(url)) %>%
-  pull(url)
 
 if (! "pg_party_color.csv" %in% list.files()) {
   
+  # Packages ----------------------------------------------------------------
+  library(tidyverse)
+  library(partycoloR) # devtools::install_github("lwarode/partycoloR")
+  
+  
+  # Party Facts ParlGov -----------------------------------------------------
+  pf_url <- "https://partyfacts.herokuapp.com/download/external-parties-csv/"
+  
+  pf_raw <- read_csv(pf_url, guess_max = 30000)
+  
+  pf_pg <- pf_raw %>% 
+    filter(dataset_key == "parlgov")
+  
+  
+  # Wikipedia Party URLs ----------------------------------------------------
+  wiki_url <- "https://raw.githubusercontent.com/hdigital/partyfactsdata/master/import/wikipedia/wikipedia.csv"
+  
+  wiki_raw <- read_csv(wiki_url)
+  
+  wiki_pg <- wiki_raw %>% 
+    select(partyfacts_id, url) %>% 
+    right_join(pf_pg, by = "partyfacts_id")
+  
+  wiki_pg_list <-  wiki_pg %>%
+    filter(! is.na(url)) %>%
+    pull(url)
+  
+
+  # partycoloR --------------------------------------------------------------
   pg_party_color_raw <- wikipedia_party_color(wiki_pg_list)
   
   pg_party_color <- pg_party_color_raw %>% 
